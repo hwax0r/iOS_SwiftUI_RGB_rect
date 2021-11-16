3c6f61e2706715b5a82a6e1235d2b5315d1d0581
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-16 17:05:59 +0200

    Just trying to add new view

--- RGBApp/ContentView.swift
+++ RGBApp/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, Swift!")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
+    }
+}
+
+struct SecondContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("Second screen")
+            .previewDevice("iPhone 12")
     }
 }
